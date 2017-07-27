fef8e3af0525b8af30ea7b8cd075961a086e5a43
Rabugari <takaramassao@hotmail.com>
2017-07-27 20:49:03 -0300

    adicionando mais casos de testes

--- src/br/com/caixa/CaixaEletronico.java
+++ src/br/com/caixa/CaixaEletronico.java
@@ -3,6 +3,7 @@ package br.com.caixa;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static br.com.caixa.enums.Nota.*;
 
@@ -26,33 +27,44 @@ public class CaixaEletronico {
 	public List<Nota> saca(long quantidadeDoSaque) throws IllegalArgumentException, CaixaSemNotasException {
 		List<Nota> notasDoSaque = new ArrayList<>();
 
+		Map<Nota, Long> notasEmTransacao = notas;
+		
 		if(quantidadeDoSaque < 0)
 			throw new IllegalArgumentException("Não se pode sacar um valor negativo");
 		
 		while(quantidadeDoSaque>0){
+			
+			if(verificaSeCaixaEstaVazio(notasEmTransacao))
+				throw new CaixaSemNotasException("O Caixa não possui todo este dinheiro");
+			
 			if(notaEhValida(quantidadeDoSaque,DEZ, VINTE))
-				quantidadeDoSaque = alteraSaldo(quantidadeDoSaque, DEZ, notasDoSaque);
+				quantidadeDoSaque = alteraSaldo(quantidadeDoSaque, DEZ, notasDoSaque, notasEmTransacao);
 
 			if(notaEhValida(quantidadeDoSaque, VINTE, CINQUENTA))
-				quantidadeDoSaque = alteraSaldo(quantidadeDoSaque, VINTE, notasDoSaque);
+				quantidadeDoSaque = alteraSaldo(quantidadeDoSaque, VINTE, notasDoSaque, notasEmTransacao);
 
 			if(notaEhValida(quantidadeDoSaque, CINQUENTA, CEM))
-				quantidadeDoSaque = alteraSaldo(quantidadeDoSaque, CINQUENTA, notasDoSaque);
+				quantidadeDoSaque = alteraSaldo(quantidadeDoSaque, CINQUENTA, notasDoSaque, notasEmTransacao);
 
 			if((quantidadeDoSaque >= CEM.getValor()) && getQuantidadeDeNotas(CEM) > 0)
-				quantidadeDoSaque = alteraSaldo(quantidadeDoSaque, CEM, notasDoSaque);
+				quantidadeDoSaque = alteraSaldo(quantidadeDoSaque, CEM, notasDoSaque, notasEmTransacao);
 		}
+		
+		finalizaTransacao(notasEmTransacao);
+		
 		return notasDoSaque;
 	}
 
-	private boolean notaEhValida(long quantidadeDoSaque, Nota notaMenor, Nota notaMaior) throws CaixaSemNotasException{
-		return ((quantidadeDoSaque >= notaMenor.getValor() && quantidadeDoSaque < notaMaior.getValor()) && getQuantidadeDeNotas(notaMenor) > 0);
+	private boolean notaEhValida(final long quantidadeDoSaque, final Nota notaMenor, final Nota notaMaior) throws CaixaSemNotasException{
+		return (getQuantidadeDeNotas(notaMaior) == 0 || quantidadeDoSaque < notaMaior.getValor())
+				&& getQuantidadeDeNotas(notaMenor) > 0  
+				&& quantidadeDoSaque >= notaMenor.getValor();
 	}
 	
-	private long alteraSaldo(long quantidadeDoSaque, Nota notaASerSacada, List<Nota> notasDoSaque){
+	private long alteraSaldo(long quantidadeDoSaque, final Nota notaASerSacada, List<Nota> notasDoSaque, Map<Nota,Long> notasEmTransacao){
 		notasDoSaque.add(notaASerSacada);
-		long quantidade = notas.get(notaASerSacada);
-		notas.put(notaASerSacada, new Long(--quantidade));
+		long quantidade = notasEmTransacao.get(notaASerSacada);
+		notasEmTransacao.put(notaASerSacada, new Long(--quantidade));
 		
 		quantidadeDoSaque -= notaASerSacada.getValor();
 		
@@ -74,6 +86,15 @@ public class CaixaEletronico {
 			return quantidade!= null ? quantidade.longValue() : 0;
 		}
 		throw new CaixaSemNotasException("O Caixa está vazio.");
+	}
+	
+	private boolean verificaSeCaixaEstaVazio(Map<Nota, Long> notasEmTransacao){
+		Stream<Long> filter = notasEmTransacao.values().stream().filter(nota -> nota.longValue() > 0L);
+		return filter.count() ==0;
+	}
+	
+	private void finalizaTransacao(Map<Nota, Long> notasEmTransacao) {
+		this.notas = notasEmTransacao;
 	}
 
 	public final Map<Nota, Long> getNotas() {
--- srcTest/br/com/caixa/CaixaEletronicoTest.java
+++ srcTest/br/com/caixa/CaixaEletronicoTest.java
@@ -22,7 +22,7 @@ import br.com.caixa.exceptions.CaixaSemNotasException;
 @RunWith(JUnit4.class)
 public class CaixaEletronicoTest {
 
-	private CaixaEletronico caixa;
+	private CaixaEletronico caixaEletronico;
 	
 	@Before
 	public void setup(){
@@ -32,35 +32,46 @@ public class CaixaEletronicoTest {
 		notas.put(CINQUENTA, 1L);
 		notas.put(CEM, 3L);
 		
-		this.caixa = new CaixaEletronico(notas);
+		this.caixaEletronico = new CaixaEletronico(notas);
 	}
 	
 	@Test
 	public void verificaQuantidadeAposAdicionarNotaDe10() throws CaixaSemNotasException{
-		caixa.deposita(DEZ);
-		assertEquals(caixa.getQuantidadeDeNotas(DEZ), 6, 6);
+		caixaEletronico.deposita(DEZ);
+		assertEquals(caixaEletronico.getQuantidadeDeNotas(DEZ), 6, 6);
 	}
 	
 	@Test
-	public void saqueDe40() throws IllegalArgumentException, CaixaSemNotasException{
-		List<Nota> notasRetornadas = caixa.saca(40);
+	public void saqueDe60() throws IllegalArgumentException, CaixaSemNotasException{
+		List<Nota> notasRetornadas = caixaEletronico.saca(40);
 		assertEquals(Arrays.asList(VINTE, VINTE), notasRetornadas);
 	}
 	
 	@Test
-	public void saqueDe140() throws IllegalArgumentException, CaixaSemNotasException{
-		List<Nota> notasRetornadas = caixa.saca(140);
-		assertEquals(Arrays.asList(CEM, VINTE, VINTE), notasRetornadas);
+	public void saqueDe170() throws IllegalArgumentException, CaixaSemNotasException{
+		List<Nota> notasRetornadas = caixaEletronico.saca(170);
+		assertEquals(Arrays.asList(CEM, CINQUENTA, VINTE), notasRetornadas);
 	}
 	
 	@Test
-	public void saqueDe350() throws IllegalArgumentException, CaixaSemNotasException{
-		List<Nota> notasRetornadas = caixa.saca(350);
-		assertEquals(Arrays.asList(CEM, CEM, CEM, CINQUENTA), notasRetornadas);
+	public void saqueDe380() throws IllegalArgumentException, CaixaSemNotasException{
+		List<Nota> notasRetornadas = caixaEletronico.saca(380);
+		assertEquals(Arrays.asList(CEM, CEM, CEM, CINQUENTA, VINTE, DEZ), notasRetornadas);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void tentaSacarValorInvalido() throws IllegalArgumentException, CaixaSemNotasException{
-		caixa.saca(-50);
+		caixaEletronico.saca(-50);
+	}
+	
+	@Test
+	public void verificaQuantidadeDeNotasAposOSaque() throws CaixaSemNotasException {
+		caixaEletronico.saca(100);
+		assertEquals(caixaEletronico.getQuantidadeDeNotas(CEM), 2L);
+	}
+	
+	@Test(expected = CaixaSemNotasException.class)
+	public void sacouMaisQueLimite() throws CaixaSemNotasException{
+		caixaEletronico.saca(450);
 	}
 }
